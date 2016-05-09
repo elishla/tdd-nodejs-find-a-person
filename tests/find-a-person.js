c1fa8ec42236b02cdbc61b4ccc327ab6ce407da2
elishla <elcae88@gmail.com>
2016-05-09 16:51:02 +0300

    GREEN:returns posts of a person that was found

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -9,3 +9,14 @@ describe('Find a person', function() {
     expect(posts).to.be.eql([]);
   });
 });
+
+describe('Find a person', function() {
+  it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
+    var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
+    var posts = map.find_a_person("Or A.")
+    return posts;
+    //expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+  });
+});
+
+
