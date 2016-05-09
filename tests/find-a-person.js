87ef20427e01d49003c1b271478e2ce9f45df949
elishla <elcae88@gmail.com>
2016-05-09 16:52:24 +0300

    GREEN:organizing code

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -8,9 +8,7 @@ describe('Find a person', function() {
     var posts = map.find_a_person("beny")
     expect(posts).to.be.eql([]);
   });
-});
 
-describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
     var posts = map.find_a_person("Or A.")
