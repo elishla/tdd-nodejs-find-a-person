c9520c4ba7320f2728c2c7e0b41a57f853ed94ef
orayr <orajerusalem1995@gmail.com>
2016-05-16 17:06:22 +0300

    RED: one is wrong

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -5,7 +5,7 @@ var Map = require('./../src/map');
 describe('Find a person', function() {
   it('Given a person name, return all posts (of a map) containing her name (in any of a post fields)', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
-    var posts = map.find_a_person("beny")
+    var posts = map.find_a_person("Or A.")
     expect(posts).to.be.eql([]);
   });
 
@@ -13,7 +13,7 @@ describe('Find a person', function() {
     var map = new Map(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "Random post"]);
     var posts = map.find_a_person("Or A.")
     Map(posts);
-    //expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
+    expect(posts).to.be.eql(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"]);
   });
 });
 
