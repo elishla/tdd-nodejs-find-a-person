b1b3ff65678387d3b1f435d95072cb80d5b4b913
orayr <orajerusalem1995@gmail.com>
2016-05-16 17:37:36 +0300

    RED: the function find person not working

--- src/map.js
+++ src/map.js
@@ -4,7 +4,14 @@ function Map(posts) {
 }
 
 Map.prototype.find_a_person = function(name) {
-  return [];
+
+      var arrayPosts =[];
+      for (var index = 0; this._posts.length < len; index++) {
+
+          if(this._posts[index].indexOf(name)!=-1)
+             arrayPosts.push(this._posts[index]);
+      }
+  return arrayPosts;
   };
 
 Map.prototype.find_if_person_exists = function(name) {
