a328a2ced2340ed68b826253ad29722e34b92cdf
elishla <elcae88@gmail.com>
2016-05-23 17:56:39 +0300

    the location validation is working

--- src/map.js
+++ src/map.js
@@ -36,4 +36,23 @@ Map.prototype.find_if_persons_location = function(name) {
   return arrayPosts;
   };
 
+
+Map.prototype.find_if_valid_location = function(name) {
+
+   var arrayPosts =[];
+     var j=0;
+      for (var index = 0; index < this._posts.length; index++) {
+
+          if(this._posts[index].indexOf(name)!=-1){
+
+              j++;
+          }
+
+      }
+   if(j>1)
+  return true;
+    return false;
+  };
+
+
 module.exports = Map;
